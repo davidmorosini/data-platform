70ce7d733cddf52ef497957a0abbc66902f1f172
davidmorosini <david.morosinineto@gmail.com>
2021-11-18 15:07:01 -0300

    feat: read yaml file plugin

--- plugins/shared/io/files.py
+++ plugins/shared/io/files.py
@@ -1,8 +1,18 @@
+import json
 from pathlib import Path
+from typing import List
+import yaml
 
 
-def read_file(filename: Path) -> str:
-    """Read files function"""
-    with open(filename, "r") as f:
+def read_file_to_str(file_path: Path, mode: str = "r") -> str:
+    with open(file_path, mode) as f:
         content = f.read()
     return content
+
+
+def read_yaml_file_to_dict(yaml_file_path: Path, mode="r") -> dict:
+    with open(yaml_file_path, mode) as f:
+        yaml_file = yaml.safe_load(f)
+    yaml_dumped = json.dumps(yaml_file, default=str)
+    dict_loaded = json.loads(yaml_dumped)
+    return dict_loaded
